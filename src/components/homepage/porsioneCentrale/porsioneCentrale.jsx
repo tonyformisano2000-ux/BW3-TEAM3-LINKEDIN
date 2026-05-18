50010ca56b11618c82475048df442231c5003efd
Antonio <tony.formisano.2000@gmail.com>
2026-05-18 14:44:06 +0200

    crea un post

---
+++ src/components/homepage/porsioneCentrale/porsioneCentrale.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+import CreaUnPost from "./CreaUnPost";
+
+const PorzioneCentrale = () => {
+  return (
+    <Container>
+      <CreaUnPost />
+    </Container>
+  );
+};
+export default PorzioneCentrale;
