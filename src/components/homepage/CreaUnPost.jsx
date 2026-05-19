fd8d14f9ae93e2cbc90c281286f17471f40f5f0c
Amir B. Kahna <amir.kahna2@gmail.com>
2026-05-19 19:07:23 +0200

    Fixed UI

--- src/components/homepage/CreaUnPost.jsx
+++ src/components/homepage/CreaUnPost.jsx
@@ -1,35 +1,44 @@
-import { Container, Row } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 import { BlockquoteLeft, PlayBtnFill, Image } from "react-bootstrap-icons";
 
 const CreaUnPost = () => {
   return (
-    <Container className="bg-white rounded-2 shadow-sm p-4 mb-2">
-      <Row className="mt-3 mb-1">
-        <img
-          className="rounded-circle m-1 col-1 img-fluid"
-          src="https://placecats.com/200/200"
-        ></img>
-        <input
-          type="text"
-          placeholder="Crea un post"
-          className=" border-2 text-dark-subtle border-black border-opacity-75 rounded-5 col-10 fw-bold"
-        ></input>
-      </Row>
-      <Row>
-        <div className="border-box rounded-2 col-4 bg-white d-flex align-items-center justify-content-center my-2 p-1">
-          <PlayBtnFill className="text-success fs-4 mx-1" />
-          <p className="text-dark-subtle fw-bold fs-6 mb-0">Video</p>
-        </div>
-        <div className="border-box rounded-2 col-4 bg-white d-flex align-items-center justify-content-center my-2 p-1">
-          <Image className="text-primary  fs-4 mx-1" />
-          <p className="text-dark-subtle fw-bold fs-6 mb-0">Foto</p>
-        </div>
-        <div className=" border-box rounded-2 col-4 bg-white d-flex align-items-center justify-content-center my-2 p-1">
-          <BlockquoteLeft className=" text-warning fs-4 mx-1" />
-          <p className="text-dark-subtle fw-bold fs-6 mb-0">Scrivi articolo</p>
-        </div>
-      </Row>
-    </Container>
+    <Row className="align-items-center mb-3 p-3 bg-white rounded-2 shadow-sm">
+      <div className="col-11">
+        <Row className="mb-3">
+          <div className="d-flex">
+            <img
+              className="col-1 rounded-circle img-fluid me-3"
+              src="https://placecats.com/200/200"
+              alt="user"
+            />
+            <input
+              type="text"
+              placeholder="Crea un post"
+              className="form-control rounded-pill fw-bold py-2"
+            />
+          </div>
+        </Row>
+
+        <Row className="text-center">
+          <div className="col-4 d-flex justify-content-center align-items-center">
+            <PlayBtnFill className="text-success fs-4 me-2" />
+            <span className="fw-bold text-muted">Video</span>
+          </div>
+
+          <div className="col-4 d-flex justify-content-center align-items-center">
+            <Image className="text-primary fs-4 me-2" />
+            <span className="fw-bold text-muted">Foto</span>
+          </div>
+
+          <div className="col-4 d-flex justify-content-center align-items-center">
+            <BlockquoteLeft className="text-warning fs-4 me-2" />
+            <span className="fw-bold text-muted">Scrivi articolo</span>
+          </div>
+        </Row>
+      </div>
+    </Row>
   );
 };
+
 export default CreaUnPost;
