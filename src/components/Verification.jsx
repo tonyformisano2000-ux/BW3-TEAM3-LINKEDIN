75ef1f8c995305c911dda6b82eee13c4e48256cf
Amir B. Kahna <amir.kahna2@gmail.com>
2026-05-20 16:31:22 +0200

    Added qr code

--- src/components/Verification.jsx
+++ src/components/Verification.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const Verification = () => {
   return (
     <div className="d-flex justify-content-center">
-      <div className="w-md-50 bg-light rounded-2 shadow-sm d-flex flex-column p-3 mt-5 mx-3 mx-md-0">
+      <div className="w-md-50 bg-light rounded-2 shadow-sm d-flex flex-column p-4 mt-5 mx-3 mx-md-0 ">
         <Link to="/home" className="text-decoration-none">
           <i class="bi bi-arrow-left text-secondary">Go back</i>
         </Link>
@@ -22,21 +22,21 @@ export const Verification = () => {
         <div className="d-flex">
           <div>
             <img
-              src="src\assets\LinkedIn_icon.svg.png"
+              src="src\assets\qrcode.png"
               alt="qrcode"
-              style={{ width: "150px" }}
+              style={{ width: "200px" }}
             />
           </div>
           <div className="ms-3">
             <p className="fw-bold">
               Instructions: <br />
               <span className="fw-normal">
-                1. Open the camera app on your phone.{" "}
-              </span>{" "}
+                1. Open the camera app on your phone.
+              </span>
               <br />
               <span className="fw-normal">
                 2. Scan the QR code on the left.
-              </span>{" "}
+              </span>
               <br />
               <span className="fw-normal">
                 3. Follow the steps on your phone.
@@ -45,8 +45,8 @@ export const Verification = () => {
           </div>
         </div>
         <p className="mt-2">
-          Learn{" "}
-          <span className="text-primary fw-bold">
+          Learn
+          <span className="text-primary fw-bold ms-1 pointer">
             how users can verify their information.
           </span>
         </p>
